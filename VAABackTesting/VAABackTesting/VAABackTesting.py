f31f6246f67b00c01569631e676ca6b6d888dd5d
GoodayTH <taehyeongkim77@gmail.com>
2021-11-13 16:22:03 +0900

    VAA-3 : 말일 데이터만 뽑게 수정

--- VAABackTesting/VAABackTesting/VAABackTesting.py
+++ VAABackTesting/VAABackTesting/VAABackTesting.py
@@ -30,15 +30,15 @@ def main():
     end_day = datetime.date(2021,11,13) # 종료일 
 
     # 공격자산 
-    SPY = pdr.get_data_yahoo('SPY', start_day, end_day)['Adj Close'] 
-    VEA = pdr.get_data_yahoo('VEA', start_day, end_day)['Adj Close'] 
-    EEM = pdr.get_data_yahoo('EEM', start_day, end_day)['Adj Close'] 
-    AGG = pdr.get_data_yahoo('AGG', start_day, end_day)['Adj Close'] 
+    SPY = pdr.get_data_yahoo('SPY', start_day - datetime.timedelta(days=365), end_day)['Adj Close'] 
+    VEA = pdr.get_data_yahoo('VEA', start_day - datetime.timedelta(days=365), end_day)['Adj Close'] 
+    EEM = pdr.get_data_yahoo('EEM', start_day - datetime.timedelta(days=365), end_day)['Adj Close'] 
+    AGG = pdr.get_data_yahoo('AGG', start_day - datetime.timedelta(days=365), end_day)['Adj Close'] 
 
     # 수비자산 
-    LQD = pdr.get_data_yahoo('LQD', start_day, end_day)['Adj Close'] 
-    SHY = pdr.get_data_yahoo('SHY', start_day, end_day)['Adj Close'] 
-    IEF = pdr.get_data_yahoo('IEF', start_day, end_day)['Adj Close']
+    LQD = pdr.get_data_yahoo('LQD', start_day - datetime.timedelta(days=365), end_day)['Adj Close'] 
+    SHY = pdr.get_data_yahoo('SHY', start_day - datetime.timedelta(days=365), end_day)['Adj Close'] 
+    IEF = pdr.get_data_yahoo('IEF', start_day - datetime.timedelta(days=365), end_day)['Adj Close']
 
     df_VAA = pd.concat([SPY,VEA,EEM,AGG,LQD,SHY,IEF],axis=1) 
     df_VAA.columns = ['SPY','VEA','EEM','AGG','LQD','SHY','IEF'] 
@@ -46,9 +46,16 @@ def main():
 
     # 각 자산별 모멘텀 지수 계산 
     df_VAA[['SPY_M','VEA_M','EEM_M','AGG_M','LQD_M','SHY_M','IEF_M']] = df_VAA.apply(lambda x: get_momentum(df_VAA, x), axis=1) 
-    df_VAA.tail(10)
+    #df_VAA.tail(10)
 
-    print(df_VAA) # print to Console
+    # 백테스트할 기간 데이터 추출 
+    df_VAA = df_VAA[start_day:end_day] 
+    
+    # 매월 말일 데이터만 추출(리밸런싱에 사용) 
+    df_VAA = df_VAA.resample(rule='M').apply(lambda x: x[-1]) 
+    df_VAA.head(10)
+
+    #print(df_VAA) # print to Console
     df_VAA.to_csv('./outPut.csv', sep=',', na_rep="NaN") # printf to File
 
 
