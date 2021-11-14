fafe67aebf13dd37c7eadeb92469bad506dec80d
GoodayTH <taehyeongkim77@gmail.com>
2021-11-14 20:59:00 +0900

    bullMargetTester-2

--- bullMargetTester/bullMargetTester/bullMargetTester.py
+++ bullMargetTester/bullMargetTester/bullMargetTester.py
@@ -1,8 +1,10 @@
-
 import sys
+import pybithumb
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import *
 
+tickers = ["BTC", "ETH", "BCH", "ETC"]
 form_class = uic.loadUiType("./bullMargetTester.ui")[0]
 
 class MyWindow(QMainWindow, form_class):
@@ -10,7 +12,36 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
 
+        timer = QTimer(self)
+        timer.start(5000)
+        timer.timeout.connect(self.timeout)
+
+    def get_market_infos(self, ticker):
+        df = pybithumb.get_ohlcv(ticker)
+        ma5 = df['close'].rolling(window=5).mean()
+        last_ma5 = ma5[-2]
+        price = pybithumb.get_current_price(ticker)
+
+        state = None
+        if price > last_ma5:
+            state = "상승장"
+        else:
+            state = "하락장"
+
+        return price, last_ma5, state
+
+    def timeout(self):
+        for i, ticker in enumerate(tickers):
+            item = QTableWidgetItem(ticker)
+            self.tableWidget.setItem(i, 0, item)
+
+            price, last_ma5, state = self.get_market_infos(ticker)
+            self.tableWidget.setItem(i, 1, QTableWidgetItem(str(price)))
+            self.tableWidget.setItem(i, 2, QTableWidgetItem(str(last_ma5)))
+            self.tableWidget.setItem(i, 3, QTableWidgetItem(state))
+
+
 app = QApplication(sys.argv)
-win = MyWindow()
-win.show()
+window = MyWindow()
+window.show()
 app.exec_()
