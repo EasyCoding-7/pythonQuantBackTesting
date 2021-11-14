83ae678d20d4c170e75791202ec4f5339fb5d454
GoodayTH <taehyeongkim77@gmail.com>
2021-11-14 12:11:31 +0900

    Qt-Designer-2

--- PyQt/PyQt/PyQt.py
+++ PyQt/PyQt/PyQt.py
@@ -8,6 +8,10 @@ class MyWindow(QMainWindow, from_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.eventBtn.clicked.connect(self.btn_clicked)
+
+    def btn_clicked(self):
+        print("버튼 클릭")
 
 app = QApplication(sys.argv)
 window = MyWindow()
